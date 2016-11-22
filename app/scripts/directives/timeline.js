529b1150622c6142a4bf18b372306bad5c42e59a
mikima <mikimauri@gmail.com>
2016-11-22 16:47:00 +0100

    changed colors and label

--- app/scripts/directives/timeline.js
+++ app/scripts/directives/timeline.js
@@ -100,7 +100,7 @@ angular.module('fireInterfaceApp')
           .attr("x",d3.min([posl,posr]))
           .attr("y",0)
           .attr("width",Math.abs(posl-posr))
-          .style("fill","#BA5D4A")
+          .style("fill","#A4B3BC")
           .style("stroke","none")
           .style("opacity",0.2)
           .attr("height",height);
